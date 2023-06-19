e97cb47a914e9d4b3182a222e01ef8233c2819a6
Prasad Parik <prasadparik18@gmail.com>
2023-06-19 16:01:43 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,109 @@
+let form = document.getElementById("AddForm");
+let List = document.getElementById("List");
+
+// Form submit event
+form.addEventListener("submit", AddNewUser);
+
+// Delete Event
+List.addEventListener("click", deleteItem);
+
+// Edit Event
+List.addEventListener("click", editItem);
+
+// AddNewUser funct.. =====================================
+
+function AddNewUser(e) {
+  e.preventDefault();
+
+  // Get input values
+  var userName = document.getElementById("userName").value;
+  var userEmail = document.getElementById("userEmail").value;
+
+  let li = document.createElement("li");
+  li.className = "list-group-item p-2";
+
+  li.appendChild(document.createTextNode(`${userName} `));
+  li.appendChild(document.createTextNode(`${userEmail}`));
+
+  //   Add UID to user =====================================
+
+  let UID = document.createElement("id");
+  UID.style.display = "none";
+  UID.appendChild(document.createTextNode(userEmail));
+  li.appendChild(UID);
+
+  //   create Delete btn =================================
+
+  let deleteBtn = document.createElement("delete");
+  deleteBtn.className = "float-end btn bg-danger text-white delete";
+  deleteBtn.appendChild(document.createTextNode("Delete"));
+  li.appendChild(deleteBtn);
+
+  //   create Edit btn =================================
+
+  let EditBtn = document.createElement("edit");
+  EditBtn.className = "float-end btn bg-dark text-white me-1 edit";
+  EditBtn.appendChild(document.createTextNode("Edit"));
+  li.appendChild(EditBtn);
+
+  List.appendChild(li);
+
+  //   Adding user to localStorage
+
+  let userObj = {
+    name: userName,
+    email: userEmail,
+  };
+
+  let Json_UserObj = JSON.stringify(userObj);
+  localStorage.setItem(userEmail, Json_UserObj);
+
+  //   Add to CRUD CRUD API
+  axios
+    .post(
+      "https://crudcrud.com/api/a86ef64adc2e41488d520658dd17ce7a/AppointmentData",
+      userObj
+    )
+    .then((resolve) => console.log(resolve))
+    .catch((err) => console.log(err));
+
+  //   Clear input fields
+  form.userName.value = "";
+  form.userEmail.value = "";
+}
+
+// Delete Item ===============================
+
+function deleteItem(e) {
+  if (e.target.classList.contains("delete"))
+    if (confirm("Are you sure you want to delete ?"))
+      var li = e.target.parentElement;
+  List.removeChild(li);
+
+  //   removing item from local storage
+
+  localStorage.removeItem(li.childNodes[2].innerText);
+}
+
+// Edit Item ===============================
+
+function editItem(e) {
+  if (e.target.classList.contains("edit"))
+    if (confirm("Are you sure you want to edit ?"))
+      var li = e.target.parentElement;
+
+  //   getting user from local storage
+
+  let obj = JSON.parse(localStorage.getItem(li.childNodes[2].innerText));
+  console.log("OBJ:", obj);
+
+  //   filling input values
+  form.userName.value = obj.name;
+  form.userEmail.value = obj.email;
+
+  List.removeChild(li);
+
+  //   removing item from local storage
+
+  localStorage.removeItem(li.childNodes[2].innerText);
+}
